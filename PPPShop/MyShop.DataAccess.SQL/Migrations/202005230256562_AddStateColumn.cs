5872bd1e4f521e58aa42ed5506dd3cc40241a062
TriplePBallesteros <atballesteros03@yahoo.com>
2020-05-23 13:35:06 +0800

    Registering Customers

---
+++ PPPShop/MyShop.DataAccess.SQL/Migrations/202005230256562_AddStateColumn.cs
@@ -0,0 +1,18 @@
+﻿namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddStateColumn : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "State", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "State");
+        }
+    }
+}
